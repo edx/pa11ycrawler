dc134604c824d53f2cb6d75076024754cb2cd7ab
David Baumgold <david@davidbaumgold.com>
2016-08-15 09:37:13 -0400

    Increment project version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_requirements(path):
 
 setup(
     name='pa11ycrawler',
-    version='0.0.4',
+    version='1.0.0',
     package_data={
         'pa11ycrawler': [
             'templates/*.*',
