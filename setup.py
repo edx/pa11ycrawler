2560056923a7c4e014d2a271b3ee75b57292379a
David Baumgold <david@davidbaumgold.com>
2016-08-29 09:26:42 -0400

    version 1.3.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_requirements(path):
 
 setup(
     name='pa11ycrawler',
-    version='1.2.0',
+    version='1.3.0',
     package_data={
         'pa11ycrawler': [
             'templates/*.*',
