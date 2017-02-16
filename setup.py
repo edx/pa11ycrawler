101898608516587cddfbb506f25e65b1addb5076
Michael Youngstrom <myoungstrom@edx.org>
2017-02-16 12:27:20 -0500

    Bumping version to v1.5.9

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-VERSION = '1.5.8'
+VERSION = '1.5.9'
 DESCRIPTION = 'A Scrapy spider for a11y auditing Open edX installations.'
 LONG_DESCRIPTION = """pa11ycrawler is a Scrapy spider that runs a Pa11y check
 on every page of an Open edX installation,
